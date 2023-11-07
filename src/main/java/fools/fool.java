b79d7f20d2a293dd7089240cee166dec15f15829
Milko Yanakiev <myanakiev@gmail.com>
2023-11-07 10:31:39 +0200

    Add enum Ho

---
+++ src/main/java/fools/fool.java
@@ -0,0 +1,14 @@
+package fools;
+
+public class fool {
+    public static void main(String[] args) {
+        Ho dsa = Ho.DSA;
+
+        System.out.println(dsa.toString());
+        System.out.println(dsa);
+    }
+
+    public enum Ho {
+        ABC, DSA;
+    }
+}

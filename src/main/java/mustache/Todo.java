0261ddc6e343e2c88f7e13fb29db0305a6241df0
Milko Yanakiev <myanakiev@gmail.com>
2024-04-17 17:03:42 +0300

    Mustache added.

---
+++ src/main/java/mustache/Todo.java
@@ -0,0 +1,57 @@
+package mustache;
+
+import java.util.Date;
+
+public class Todo {
+
+    public Todo(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+
+    private String title;
+    private String text;
+    private boolean done;
+    private Date createdOn;
+    private Date completedOn;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Date getCompletedOn() {
+        return completedOn;
+    }
+
+    public void setCompletedOn(Date completedOn) {
+        this.completedOn = completedOn;
+    }
+}
